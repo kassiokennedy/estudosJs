af1daa7103bd4f9207a96bf6ea0871a40f1b4b9d
kassio kennedy <kassio1206@hotmail.com>
2022-04-11 17:52:52 -0300

    aula

---
+++ main.js
@@ -0,0 +1,55 @@
+//alert('Ola mundo')
+/*
+function ola(x) {
+  var f = document.getElementById('frm')
+  var txt = f.txtnome
+  txt.value = 'Ola ' + txt.value
+}
+
+function soma() {
+  //alert("1");
+  var f = document.getElementById('frm')
+  var n1 = document.getElementById('n1').value
+  var n2 = document.getElementById('n2').value
+  var soma = n1 + n2
+  alert('soma')
+}
+
+function subtracao() {
+  var f = document.getElementById('frm')
+  var n1 = document.getElementById('n1').value
+  var n2 = document.getElementById('n2').value
+  var soma = n1 - n2
+  alert(soma)
+}
+
+function multiplicacao() {
+  var f = document.getElementById('frm')
+  var n1 = document.getElementById('n1').value
+  var n2 = document.getElementById('n2').value
+  var soma = n1 * n2
+  alert(soma)
+}
+
+function divisao() {
+  var f = document.getElementById('frm')
+  var n1 = document.getElementById('n1').value
+  var n2 = document.getElementById('n2').value
+  var soma = n1 / n2
+  alert(soma)
+}
+
+function mostrarresultados() {}
+
+function limpar() {
+  //alert("1");
+  var f = document.getElementById('frm')
+  var nome1 = f.txtnome
+  var n1 = f.n1
+  var n2 = f.n2
+
+  nome1.value = ''
+  n1.value = ''
+  n2.value = ''
+}
+*/
